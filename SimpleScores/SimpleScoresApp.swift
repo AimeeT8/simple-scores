32af7432ced03fb3142b836afabf1ad48dd86af8
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-09-25 13:30:57 -0400

    Initial Commit

---
+++ SimpleScores/SimpleScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleScoresApp.swift
+//  SimpleScores
+//
+//  Created by Aimee Temple on 2025-09-25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
