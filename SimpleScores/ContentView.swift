ed72c8737054bf9b947912afc1c2f36f8af7a2f3
Aimee Temple <52682021+AimeeT8@users.noreply.github.com>
2025-09-25 15:01:59 -0400

    Add ToolbarItems to reset the score and the delete all players. Add an alert asking the player if they really want to do it.

--- SimpleScores/ContentView.swift
+++ SimpleScores/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     
     @ObservedObject var model: ViewModel
     
+    @State private var showingClearAlert = false
+    
+    
     var body: some View {
         List {
             ForEach($model.items, content: ScoreRow.init)
@@ -25,6 +28,29 @@ struct ContentView: View {
             // don't add more players than there are colors.
             .disabled(model.items.count == ColorChoice.allCases.count)
             .listRowSeparator(.hidden)
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: model.reset) {
+                    Label("Reset Scores to 0", systemImage: "arrow.counterclockwise")
+                }
+                .disabled(model.items.isEmpty)
+            }
+            
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button {
+                    showingClearAlert = true
+                } label: {
+                    Label("Remove All", systemImage: "trash")
+                }
+                .disabled(model.items.isEmpty)
+            }
+        }
+        .alert("Delete all players?", isPresented: $showingClearAlert) {
+            Button("Delete", role: .destructive, action: model.deleteAll)
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("This will immediately remove all players and their score.")
         }
         .animation(.default, value: model.items)
         .navigationTitle("Simple Scores")
